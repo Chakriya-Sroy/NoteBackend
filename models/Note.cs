0a0a90c366a7e34346b86f0d28c93eedc8ce17ff
chakriyasroy <sroy.c@adisoft.io>
2026-01-16 16:31:33 +0700

    update : Time

--- models/Note.cs
+++ models/Note.cs
@@ -6,7 +6,7 @@ public class Note
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Content { get; set; } = string.Empty;
-    public DateTime CreatedAt { get; set; } = DateTime.Now;
-    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 
 }
--- repositories/NoteRepository.cs
+++ repositories/NoteRepository.cs
@@ -6,7 +6,7 @@ namespace NoteBackend.Repositories
 {
     public interface INoteRepository
     {
-        Task<IEnumerable<Note>> GetAllNotesAsync(string ?search);
+        Task<IEnumerable<Note>> GetAllNotesAsync(string? search);
         Task<Note?> GetNoteByIdAsync(int id);
         Task<Note> CreateNoteAsync(Note note);
         Task<Note?> UpdateNoteAsync(int id, Note note);
@@ -23,7 +23,7 @@ namespace NoteBackend.Repositories
             _context = context;
         }
 
-        public async Task<IEnumerable<Note>> GetAllNotesAsync(string ?search)
+        public async Task<IEnumerable<Note>> GetAllNotesAsync(string? search)
         {
             var query = "SELECT * FROM Notes";
 
@@ -52,12 +52,24 @@ namespace NoteBackend.Repositories
         {
             var query = @"
                 INSERT INTO Notes (Title, Content, CreatedAt, UpdatedAt)
-                VALUES (@Title, @Content, GETDATE(), GETDATE());
+                VALUES (@Title, @Content, @CreatedAt, @UpdatedAt);
                 
                 SELECT * FROM Notes WHERE Id = SCOPE_IDENTITY();";
 
+
+            var seAsiaTime = TimeZoneInfo.ConvertTimeFromUtc(
+                DateTime.UtcNow,
+                TimeZoneInfo.FindSystemTimeZoneById("SE Asia Standard Time")
+            );
+
             using var connection = _context.CreateConnection();
-            var createdNote = await connection.QuerySingleAsync<Note>(query, note);
+            var createdNote = await connection.QuerySingleAsync<Note>(query, new
+            {
+                note.Title,
+                note.Content,
+                CreatedAt = seAsiaTime,
+                UpdatedAt = seAsiaTime
+            });
             return createdNote;
         }
 
@@ -67,16 +79,23 @@ namespace NoteBackend.Repositories
                 UPDATE Notes 
                 SET Title = @Title, 
                     Content = @Content, 
-                    UpdatedAt = GETDATE()
+                    UpdatedAt = @UpdatedAt
                 WHERE Id = @Id;
                 SELECT * FROM Notes WHERE Id = @Id;";
 
+           var seAsiaTime = TimeZoneInfo.ConvertTimeFromUtc(
+                DateTime.UtcNow,
+                TimeZoneInfo.FindSystemTimeZoneById("SE Asia Standard Time")
+            );
+
             using var connection = _context.CreateConnection();
+
             var updatedNote = await connection.QuerySingleOrDefaultAsync<Note>(query, new
             {
                 Id = id,
                 note.Title,
-                note.Content
+                note.Content,
+                UpdatedAt = seAsiaTime,
             });
 
             return updatedNote;
